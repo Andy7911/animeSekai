35918d9e6b1f1a56cc35ccfa42c97009e4fe2f4b
Anderson Beauvais <50684207+Andy7911@users.noreply.github.com>
2024-11-19 12:05:17 -0500

    sa fonctionne

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 const Index: React.FC = () => {
-  return
-   <div className='homePage'> 
-   <div className='hero'>
-    <div className='hero__left'>
+  return<div className='homePage'>
+    <div className='hero'>
+      <div className='hero__left'>
+        <h1>ggwwga</h1>
+      </div>
+      <div className='hero__right'>
 
+      </div>
     </div>
-    <div className='hero__right'>
-
-    </div>
-   </div>
 
 
   </div>;
